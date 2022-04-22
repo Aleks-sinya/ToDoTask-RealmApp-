054964ef1218adac4038ccb241c4d3ac95660db4
Aleks.sinya <sinyagovskiy93@mail.ru>
2022-04-22 17:34:21 +0700

    Initial Commit

---
+++ ToDoTask (RealmApp)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoTask (RealmApp)
+//
+//  Created by Алексей Синяговский on 22.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

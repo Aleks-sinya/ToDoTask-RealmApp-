40f94aca09588f16e3046f36680ccd983c0fe1b9
Aleks.sinya <sinyagovskiy93@mail.ru>
2022-04-24 14:26:54 +0700

    done

---
+++ ToDoTask (RealmApp)/Services/StorageManager.swift
@@ -0,0 +1,86 @@
+//
+//  StorageManager.swift
+//  ToDoTask (RealmApp)
+//
+//  Created by Алексей Синяговский on 22.04.2022.
+//
+
+import Foundation
+import RealmSwift
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    let realm = try! Realm()
+    
+    private init() {}
+    
+    // MARK: - Task List
+    func save (_ taskLists: [TaskList]) {
+        write {
+            realm.add(taskLists)
+        }
+    }
+    
+    func save(_ taskList: TaskList) {
+        write {
+            realm.add(taskList)
+        }
+    }
+    
+    func delete(_ taskList: TaskList) {
+        write {
+            realm.delete(taskList.tasks)
+            realm.delete(taskList)
+        }
+    }
+    
+    func edit(_ taskList: TaskList, newValue: String) {
+        write {
+            taskList.name = newValue
+        }
+    }
+    
+    func done(_ taskList: TaskList) {
+        write {
+            taskList.tasks.setValue(true, forKey: "isComplete")
+        }
+    }
+    
+    
+    // MARK: - Tasks
+    func save(_ task: Task, to taskList: TaskList) {
+        write {
+            taskList.tasks.append(task)
+        }
+    }
+    
+    func delete(_ task: Task) {
+        write {
+            realm.delete(task)
+        }
+    }
+    
+    func edit(_ task: Task, newValue: String, newNote: String) {
+        write {
+            task.name = newValue
+            task.note = newNote
+        }
+    }
+    
+    func done(_ task: Task, for bool: Bool) {
+        write {
+            task.setValue(bool, forKey: "isComplete")
+        }
+    }
+    
+    private func write(completion: () -> Void) {
+        do {
+            try realm.write {
+                completion()
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
